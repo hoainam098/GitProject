395e15eb9516b57376670f2dc732605c4fae2ebe
hoainam098 <nguyenhoainam098@gmail.com>
2022-01-19 04:46:15 +0900

    BrandNew

---
+++ Exercise/choose-answer.js
@@ -0,0 +1,10 @@
+// function incorrect() {
+//     alert('Đáp án bạn chọn không đúng.')
+//
+// }
+// function correct() {
+//     alert('Xin chúc mừng. Đáp án chính xác.')
+//
+// }
+// document.getElementById('a');
+// addEventListener('onclick',incorrect;)
