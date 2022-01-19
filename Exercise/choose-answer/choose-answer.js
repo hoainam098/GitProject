f14f19f00b34e1f7be0bb3602d25095d3a1817ca
hoainam098 <nguyenhoainam098@gmail.com>
2022-01-19 20:57:46 +0900

    sap xep done

---
+++ Exercise/choose-answer/choose-answer.js
@@ -0,0 +1,7 @@
+var result=document.getElementById('asw');
+        function ads() {
+        if (result.value == 3) {
+            alert('Kết quả chính xác bằng 3.')
+        } else {
+            alert('Kết quả bạn nhập chưa chính xác rồi.')
+        }}
